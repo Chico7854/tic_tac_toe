3113063824e21bc4b33c13d7e61f8473f0863aae
Lucas Tanaka <160812489+Chico7854@users.noreply.github.com>
2024-12-18 14:57:59 -0300

    Add event listeners to board and add coordinates

--- assets/scripts/Board.js
+++ assets/scripts/Board.js
@@ -5,9 +5,47 @@ export class Board {
             [0, 0, 0],
             [0, 0, 0],
         ];
+        this.mapDOMSquares();
+        this.connectBoard();
     }
 
-    updateBoard(row, column, player) {
+    mapDOMSquares() {
+        const squares = document.querySelectorAll('.square');
+        let row = 0;
+        let column = 0;
+       
+        for (const square of squares) {
+            square.dataset.coordinates = `[${row}, ${column}]`;
+            column++;
+            if (column > 2) {
+                row++;
+                column = 0;
+            }
+        }
+    }
+
+    connectBoard() {
+        const board = document.getElementById('board');
+        board.addEventListener('click', event => {
+            const target = event.target;
+            if (target.classList.contains('square')) {
+                const coordinates = JSON.parse(target.dataset.coordinates);
+                this.updateBoardData(coordinates, 1);
+                this.updateBoardUI(target);
+                console.log(this.board);
+            } else {
+                event.stopPropagation();
+            }
+        })
+    }
+
+    updateBoardData(coordinates, player) {
+        const row = coordinates[0];
+        const column = coordinates[1];
         this.board[row][column] = player;
+    }
+
+    updateBoardUI(squareElement) {
+        squareElement.style.backgroundColor = 'red';
     }
 }
--- assets/scripts/Game.js
+++ assets/scripts/Game.js
@@ -3,7 +3,6 @@ import { Board } from "./Board.js";
 class Game {
     static startGame() {
         const board = new Board();
-        const button = document.querySelector("button");
     }
 }
 
